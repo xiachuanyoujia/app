28590be9731eddf0f042ea319b7ac1f59bb17620
zengzhan <1243357202@qq.com>
2022-02-01 14:08:56 +0800

    2/1

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,32 @@ import Home from '@/pages/Home'
 import Search from '@/pages/Search'
 import Login from '@/pages/Login'
 import Register from '@/pages/Register'
+//先把VueRoutter原型对象的push,先保存一份
+let originPush = VueRouter.prototype.push;
+let originReplace = VueRouter.prototype.replace;
+
+//重写Push | replace
+//第一个参数：告诉原来push方法，你往哪里跳转（传递哪些参数）
+//第二个参数：成功回调
+//第三个参数：失败的回调
+//call || apply区别
+//相同点：都可以调用函数一次，都可以篡改函数的上下文一次
+//不同点：call与apply传递参数：call传递参数用逗号隔开，apply方法执行，传递数组
+VueRouter.prototype.push = function (location,resolve,reject){
+    if (resolve && reject) {
+        originPush.call(this,location,resolve,reject);
+    } else {
+        originPush.call(this,location,()=>{},()=>{});        
+    }
+}
+VueRouter.prototype.replace = function(location,resolve,reject){
+    if (resolve && reject) {
+        originReplace.replace(this,location,resolve,reject);
+    } else {
+        originReplace.replace(this,location,()=>{},()=>{});        
+    }
+}
+
 
 //配置路由
 export default new VueRouter({
